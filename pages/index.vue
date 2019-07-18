49650ada4bf672238f47b84ac14b5aecb1ab4c9f
mikakane <t.goto@chatbox-inc.com>
2019-07-18 21:37:39 +0900

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,68 @@
 <template>
-  <div class="container">
-     
+  <div>
+    <div class="App">
+      <header class="App-header">
+        <img src="~/assets/img/kfuglogo.png" class="App-logo" alt="logo" />
+        <p>
+          frontend conference, 2019 Nov.02 @Grandfront Osaka
+        </p>
+        <a
+                class="App-link"
+                href="https://forms.gle/YGM3jCezsgNJP9K99"
+                rel=""
+        >
+          call for paper
+        </a>
+        <a
+                class="App-link"
+                href="https://forms.gle/HLwBDwm8Wf1Pyd177"
+                rel=""
+        >
+          sponsor
+        </a>
+      </header>
+    </div>
   </div>
 </template>
 
 <script>
-
 </script>
 
 <style>
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  .App {
+    text-align: center;
+  }
+  .App-logo {
+    animation: App-logo-spin infinite 20s linear;
+    margin-top:60px;
+    height: 30vmin;
+    pointer-events: none;
+    padding: 5vmin;
+  }
+  .App-header {
+    background-color: #282c34;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+    color: white;
+  }
+  .App-header p {
+    margin:65px 0px 25px 0px;
+  }
 
+  .App-link {
+    color: #61dafb;
+    margin-bottom:10px;
+  }
 </style>
