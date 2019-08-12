0bdd3038b1e324ab5bb7ae89d995509527bc2efc
yasui05821 <yasui05821@gmail.com>
2019-08-12 13:24:06 +0900

    titleタグの調整

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || 'FRONTEND CONFERENCE 2019',
+    title: 'FRONTEND CONFERENCE 2019',
     meta: [
       {charset: 'utf-8'},
       {name: 'viewport', content: 'width=device-width, initial-scale=1'},
