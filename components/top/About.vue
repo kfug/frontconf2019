002e0667248a8ccdde45923123de1b986eab71fb
yasui05821 <yasui05821@gmail.com>
2019-08-12 14:34:33 +0900

    about　調整

--- components/top/About.vue
+++ components/top/About.vue
@@ -1,21 +1,16 @@
 <template>
   <section>
     <div class="p-about">
-      <img src="~/assets/img/about_frame01.png" class="img1" />
-      <img src="~/assets/img/about_frame01.png" class="img2" />
-      <img src="~/assets/img/circle_02.png" class="img3" />
-      <img src="~/assets/img/circle_03.png" class="img4" />
       <div class="c-container">
-        <div class="l-aboutBox">
-          <h1>About Frontend Conference 2019</h1>
-          <p>
+        <div class="p-about_intro">
+          <h1 class="p-about_secHeading">About Frontend Conference 2019</h1>
+          <p class="p-about_reed">
             2016 , 2017 , 2018年とたくさんの人に来場頂いた
-            <br />関西最大級のフロントエンド製作者向けイベント FRONTEND CONFERENCE が
-            今年も開催されます！
+            関西最大級のフロントエンド製作者向けイベント FRONTEND CONFERENCE が今年も開催されます！
             <br />
-            <br />例年通り、 デザイナもエンジニアも、職種問わず
-            <br />Webに携わるすべての人が楽しめるイベントとして
-            <br />様々なセッション・企画等を検討中です！
+            例年通り、 デザイナもエンジニアも、職種問わず
+            Webに携わるすべての人が楽しめるイベントとして
+            様々なセッション・企画等を検討中です！
           </p>
         </div>
       </div>
@@ -30,89 +25,40 @@
 @import "~/assets/scss/_library.scss";
 
 .p-about {
-  margin-top: 100px;
+  margin: 50px auto 0;
   position: relative;
-}
+  background-image:
+          url("~assets/img/about_frame01.png"),
+          url("~assets/img/about_frame02.png"),
+          url("~assets/img/circle_02.png"),
+          url("~assets/img/circle_03.png");
 
-.l-aboutBox {
-  display: inline-block;
-  background: #fafafa;
-  padding: 40px 30px;
-  margin: 50px 0px 90px 0px;
-  text-align: center;
-  position: relative;
-  z-index: 100;
+  background-size: 15%, 15%, 15%, 18%;
+  background-position: left top, right bottom, 45% 0%, 50% 100%;
+  background-repeat: no-repeat;
+
   @include desktop {
-    padding: 80px 115px;
-    max-width: 100%;
-    margin: 90px 0px 200px 0px;
+    margin-top: 100px;
   }
-  & h1 {
-    color: $clr_textLarge;
-    margin-bottom: 25px;
-    font-size: 3rem;
-    font-family: Offside;
-    font-weight: normal;
+
+  &_intro {
+    display: inline-block;
+    margin: 50px 0;
+    padding: 40px 30px;
+    width: 100%;
+    text-align: left;
+    background: $clr_textBordBg;
     @include desktop {
-      margin-bottom: 60px;
-      font-size: 4rem;
+      margin: 110px 0 200px;
+      padding: 80px 120px;
+      text-align: center;
     }
   }
-  & p {
-    font-size: 1.5rem;
-    color: $clr_textDefault;
-    @include desktop {
-      font-size: 2rem;
-    }
+  &_secHeading {
+    @include c-heading-center;
   }
-}
-
-.img1 {
-  width: 50%;
-  position: absolute;
-  left: 0px;
-  top: 0px;
-  z-index: 50;
-  @include desktop {
-    width: 20%;
-  }
-}
-.img2 {
-  width: 50%;
-  transform: scale(-1, -1);
-  filter: FlipH FlipV;
-  position: absolute;
-  right: 0px;
-  bottom: 0px;
-  z-index: 50;
-  @include desktop {
-    width: 20%;
-  }
-}
-
-.img3 {
-  width: 40%;
-  position: absolute;
-  right: 18%;
-  top: 2%;
-  z-index: 100;
-  @include desktop {
-    width: 18%;
-    right: 40%;
-  }
-}
-
-.img4 {
-  width: 43%;
-  position: absolute;
-  left: 7%;
-  bottom: 5%;
-  z-index: 80;
-  max-width: 80%;
-  @include desktop {
-    max-width: 100%;
-    width: 20%;
-    left: 5%;
+  &_reed {
+    line-height: 2;
   }
 }
 </style>
