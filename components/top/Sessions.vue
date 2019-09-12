e4fa096c10748da92c8cb8aa764508d6908ffc4f
yasui05821 <yasui05821@gmail.com>
2019-09-12 11:07:59 +0900

    追加実装用のファイル作成

---
+++ components/top/Sessions.vue
@@ -0,0 +1,66 @@
+<template>
+  <section>
+    <div class="p-about">
+      <div class="c-container">
+        <div class="p-about_intro">
+          <h1 class="p-about_secHeading">Sessions</h1>
+          <p class="p-about_read">
+            2016 , 2017 , 2018年とたくさんの人に来場頂いた
+            関西最大級のフロントエンド製作者向けイベント FRONTEND CONFERENCE が今年も開催されます！
+            <br />
+            例年通り、 デザイナもエンジニアも、職種問わず
+            Webに携わるすべての人が楽しめるイベントとして
+            様々なセッション・企画等を検討中です！
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+@import "~/assets/scss/_library.scss";
+
+.p-about {
+  margin: 50px auto 0;
+  position: relative;
+  background-image:
+          url("~assets/img/about_frame01.png"),
+          url("~assets/img/about_frame02.png"),
+          url("~assets/img/circle_02.png"),
+          url("~assets/img/circle_03.png");
+
+  background-size: 15%, 15%, 30%, 30%;
+  background-position: left top, right bottom, 45% 0%, 50% 100%;
+  background-repeat: no-repeat;
+
+  @include desktop {
+    margin-top: 100px;
+    background-size: 15%, 15%, 15%, 18%;
+  }
+
+  &_intro {
+    display: inline-block;
+    margin: 50px 0;
+    padding: 40px 30px;
+    width: 100%;
+    text-align: left;
+    background: $clr_readBordBg;
+    @include desktop {
+      margin: 110px 0 200px;
+      padding: 80px 120px;
+      text-align: center;
+    }
+  }
+  &_secHeading {
+    @include c-secHeading-center;
+
+  }
+  &_read {
+    line-height: 2;
+  }
+}
+</style>
