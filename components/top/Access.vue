bdb95d33249c40e90ac45fd0c54c9ca5e31bf3c4
mikakane <t.goto@chatbox-inc.com>
2019-10-14 15:27:03 +0900

    update

--- components/top/Access.vue
+++ components/top/Access.vue
@@ -5,15 +5,16 @@
         <div class="p-about_intro">
           <h1 class="p-about_secHeading">Access</h1>
           <p class="p-about_read">
-            会場は 梅田 グランフロント カンファレンスルーム タワーB
+            会場は 梅田 グランフロント北館10F カンファレンスルーム タワーB
             <br />
-            グランフロント内のアクセスルートは、公式サイト上の案内図を確認してください。
+            グランフロント内のアクセスルートは、公式サイト上の
+            <a href="https://www.kc-space.jp/accessmap/conference/" target="_blank">案内図</a>を確認してください。
             <br />
             <br />
           </p>
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3279.9907447540236!2d135.49244401523237!3d34.70541338043328!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6000e68e55be3c11%3A0x6d91f818ce1646c7!2z44Kw44Op44Oz44OV44Ot44Oz44OI5aSn6Ziq44K_44Ov44O8Qg!5e0!3m2!1sja!2sjp!4v1571031339384!5m2!1sja!2sjp" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
           <div class="p-about_actions">
-            <a class="p-about_btn" href="https://www.kc-space.jp/accessmap/conference/" target="_blank">アクセスマップ</a>
+            <a class="p-about_btn" href="https://www.google.com/maps?ll=34.705413,135.494633&z=16&t=m&hl=ja&gl=JP&mapclient=embed&cid=7895364407177660103" target="_blank">GoogleMapを開く</a>
           </div>
         </div>
       </div>
